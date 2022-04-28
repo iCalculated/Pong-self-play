49cacef358e4b63bc3d52cba60a741bdf6d1aeb2
iCalculated <fufa0001@gmail.com>
2022-04-28 17:18:02 -0500

    eval mods

--- eval_agents.py
+++ eval_agents.py
@@ -108,8 +108,8 @@ if __name__=="__main__":
   PATH = {
     "baseline": None,
     "ppo": "ppo1_selfplay/best_model.zip",
-    "ga": "ga_selfplay/ga_00100000.json",
-    "ga_transfer": "ga_selfplay_random/ga_00100000.json",
+    "ga_transfer": "ga_selfplay/ga_00100000.json",
+    "ga": "ga_selfplay_random/ga_00100000.json",
     "random": None,
   }
 
